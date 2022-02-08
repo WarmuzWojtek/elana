921adfc77cf0a97637d4b05e0ee027be28a59b5a
WarmuzWojtek <wwarmuz@gmail.com>
2022-02-09 00:06:59 +0100

    add mmenu light

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./scss/importer.scss";
+import MmenuLight from "mmenu-light";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = new MmenuLight(
+    document.querySelector("#menu"),
+    "(max-width: 991px)"
+  );
+
+  const navigator = menu.navigation();
+  const drawer = menu.offcanvas();
+
+  document
+    .querySelector("a[href='#menu']")
+    .addEventListener("click", (evnt) => {
+      evnt.preventDefault();
+      drawer.open();
+    });
+  document.querySelector("#menu").style.display = "block";
+});
